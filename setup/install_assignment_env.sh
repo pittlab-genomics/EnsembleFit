74499ce178c2e756a2fb91fc6a22a5c8a6fb537e
Andy Wu <49007969+sudogene@users.noreply.github.com>
2023-07-20 16:03:13 +0800

    Update install_assignment_env.sh

--- setup/install_assignment_env.sh
+++ setup/install_assignment_env.sh
@@ -10,13 +10,13 @@ if conda env list | awk '{print $1}' | grep -Eq "^${CONDA_ENV_NAME}$"; then
   conda activate $CONDA_ENV_NAME
 else
   echo "######## Conda environment not found, creating: $CONDA_ENV_NAME"
-  conda env create -n $CONDA_ENV_NAME --file ./setup/environment.yml
-#  conda create -y -n $CONDA_ENV_NAME python=3.9
+  #conda env create -n $CONDA_ENV_NAME --file ./setup/environment.yml
+  conda create -y -n $CONDA_ENV_NAME python=3.9
   conda activate $CONDA_ENV_NAME
 
 # Install conda packages using `conda install` (for first use only)
-#  conda install -y -c bioconda -c anaconda -c r -c conda-forge r=4.2 r-essentials=4.2 r-devtools=2.4.3 r-ggplot2=3.3.6 cmake=3.22.1 setuptools=65.6.3 wheel=0.38.4 r-xml r-rcpparmadillo r-gmp r-processx lapack
-#  pip install boto3==1.26.109 parsl==2023.4.17 awslambdaric==2.0.4 sigprofilerassignment==0.0.29
+  conda install -y -c bioconda -c anaconda -c r -c conda-forge r=4.2 r-essentials=4.2 r-devtools=2.4.3 r-ggplot2=3.3.6 cmake=3.22.1 setuptools=65.6.3 wheel=0.38.4 r-xml r-rcpparmadillo r-gmp r-processx lapack
+  pip install boto3==1.26.109 parsl==2023.4.17 awslambdaric==2.0.4 sigprofilerassignment==0.0.29
 fi
 
 # R-based tools
